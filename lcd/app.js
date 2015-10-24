bd01b1acca1734fc41af71968aa796f9d84ea64d
Claude Keswani <klawed@gmail.com>
2015-10-24 02:30:17 +0000

    Adding menu item to restart network

--- lcd/app.js
+++ lcd/app.js
@@ -1,8 +1,20 @@
 require('coffee-script');
 require('coffee-script/register');
 require('i2c');
+var sys = require('sys');
+var exec = require('child_process').exec;
 var LCDPLATE=require('adafruit-i2c-lcd').plate;
 var lcd= new LCDPLATE( '/dev/i2c-1', 0x20);
+
+var SELECT = 1
+var WEST = 16
+var EAST = 2
+var NORTH = 8
+var SOUTH = 4
+
+var curMessage = 0
+var messages = []
+var curIndex = 1
 function start() {
     var os = require('os');
     var ipaddress = 'unable to determine ip';
@@ -12,12 +24,56 @@ function start() {
 	ipaddress = os.networkInterfaces().wlan0[0].address;
     }
     lcd.backlight(lcd.colors.ON);
-    lcd.message(ipaddress + "\n" + new Date());
+    var reset = {}
+    reset.message = "Restart Network";
+    reset.action = function() { 
+	exec('/etc/init.d/networking restart',
+	     function (error, stdout, stderr) {
+		 var message = 'stdout: ' + stdout + '\nstderr:' + stderr
+		 console.log(message);
+		 lcd.clear();
+		 lcd.message(message);
+		 if (error !== null) {
+		     console.log('exec error: ' + error);
+		     messages.push({message:'last error' + error, action:function(){}})
+		 }
+	     });
+	console.log('Restart Network'); 
+    }
+    messages.push(reset)
+    var curaddress = {}
+    curaddress.message = ipaddress + "\n" + new Date();
+    curaddress.action = function() {console.log('curaddress');}
+    messages.push(curaddress);
+    lcd.message(messages[curMessage].message);
+    
     lcd.on('button_up', function(button) {
-	lcd.clear();
-	lcd.message('b:' + button);
+	switch (button) {
+	case NORTH:
+	    scrollup();
+	    break;
+	case SOUTH:
+	    scrolldown();
+	    break;
+	case SELECT:
+	    messages[curMessage].action();
+	}
     });
 //    process.exit(0);
+}
+
+function updateLcd() {
+        lcd.clear();
+    lcd.message(messages[curMessage].message);
+}
+function scrollup() {
+    curMessage = ++curIndex % messages.length;
+    updateLcd();
+}
+
+function scrolldown() {
+    curMessage = --curIndex % messages.length;
+    updateLcd();
 }
 
 function stop(){
